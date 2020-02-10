2393bdf934d48635ea19babfae6427924b7c1774
neytjs <35025868+neytjs@users.noreply.github.com>
2020-02-09 21:10:39 -0600

    Add files via upload

--- src/components/js/app_data.js
+++ src/components/js/app_data.js
@@ -1,15 +1,220 @@
-/*
-This class contains a single method that returns an object holding the default application's settings
-data that is inserted into their app_data.db NeDB database during their first launch of the videomanager
-application.
-*/
-
 class AppData {
   static defaultAppData() {
     return {
+      filepath: "",
       video_type: [{ url: "", label: "", width: 560, height: 315 }, { url: "https://www.youtube.com/embed/", label: "YouTube", width: 560, height: 315 }, { url: "https://player.vimeo.com/video/", label: "Vimeo", width: 640, height: 320 }],
-      video_genre: ["", "Rock", "Rap", "Pop", "Electronic", "Soundtrack", "Classical", "Parody", "Other"],
-      min_year: 1900
+      video_genre: ["Rock", "Rap", "Pop", "Electronic", "Soundtrack", "Classical", "Parody", "Other"],
+      min_year: 1900,
+      per_page: 250,
+      per_page_viewall: 200,
+      list_ids: [],
+      selected_template: "Default",
+      sel_temp_data: {
+        body_color: "#000000",
+        body_background_color: "#B8C8C8",
+        hr_color: "1px solid #000000",
+        a_color: "#045C5B",
+        a_visited_color: "#045C5B",
+        a_hover_color: "#031F1F",
+        button_background_color: "#C0C0C0",
+        button_color: "#000000",
+        button_border: "1px solid #000000",
+        button_hover_background_color: "#D0CECE",
+        button_hover_color: "#404040",
+        button_hover_border: "1px solid #808080",
+        button_active_background_color: "#808080",
+        button_active_color: "#000000",
+        button_active_border: "1px solid #000000",
+        theader_border: "2px solid #000000",
+        theader_background: "#AFD7AF",
+        entry_border: "2px solid #000000",
+        entry_background: "#C4F2DB"
+      },
+      templates: [{
+        template_name: "Default",
+        css_props: {
+          body_color: "#000000",
+          body_background_color: "#B8C8C8",
+          hr_color: "1px solid #000000",
+          a_color: "#045C5B",
+          a_visited_color: "#045C5B",
+          a_hover_color: "#031F1F",
+          button_background_color: "#C0C0C0",
+          button_color: "#000000",
+          button_border: "1px solid #000000",
+          button_hover_background_color: "#D0CECE",
+          button_hover_color: "#404040",
+          button_hover_border: "1px solid #808080",
+          button_active_background_color: "#808080",
+          button_active_color: "#000000",
+          button_active_border: "1px solid #000000",
+          theader_border: "2px solid #000000",
+          theader_background: "#AFD7AF",
+          entry_border: "2px solid #000000",
+          entry_background: "#C4F2DB"
+        }
+      }, {
+        template_name: "Green",
+        css_props: {
+          body_color: "#031F03",
+          body_background_color: "#86FA86",
+          hr_color: "1px solid #032903",
+          a_color: "#207E20",
+          a_visited_color: "#207E20",
+          a_hover_color: "#032903",
+          button_background_color: "#27A327",
+          button_color: "#032903",
+          button_border: "1px solid #032903",
+          button_hover_background_color: "#1DD31D",
+          button_hover_color: "#053F05",
+          button_hover_border: "1px solid #053F05",
+          button_active_background_color: "#053F05",
+          button_active_color: "#032903",
+          button_active_border: "1px solid #032903",
+          theader_border: "2px solid #032903",
+          theader_background: "#AFF9AF",
+          entry_border: "2px solid #032903",
+          entry_background: "#CEFCCE"
+        }
+      }, {
+        template_name: "Orange",
+        css_props: {
+          body_color: "#3D2601",
+          body_background_color: "#DDC9A9",
+          hr_color: "1px solid #3D2601",
+          a_color: "#CF6E05",
+          a_visited_color: "#CF6E05",
+          a_hover_color: "#3D2601",
+          button_background_color: "#FC9F0C",
+          button_color: "#3D2601",
+          button_border: "1px solid #3D2601",
+          button_hover_background_color: "#FCB568",
+          button_hover_color: "#CF6E05",
+          button_hover_border: "1px solid #CF6E05",
+          button_active_background_color: "#C97D05",
+          button_active_color: "#3D2601",
+          button_active_border: "1px solid #3D2601",
+          theader_border: "2px solid #3D2601",
+          theader_background: "#FEDAA0",
+          entry_border: "2px solid #3D2601",
+          entry_background: "#F0E1CA"
+        }
+      }, {
+        template_name: "Pink",
+        css_props: {
+          body_color: "#43053F",
+          body_background_color: "#FFEDFE",
+          hr_color: "1px solid #43053F",
+          a_color: "#FF1DF2",
+          a_visited_color: "#FF1DF2",
+          a_hover_color: "#43053F",
+          button_background_color: "#FF1DF2",
+          button_color: "#43053F",
+          button_border: "1px solid #43053F",
+          button_hover_background_color: "#F686ED",
+          button_hover_color: "#92128B",
+          button_hover_border: "1px solid #92128B",
+          button_active_background_color: "#92128B",
+          button_active_color: "#43053F",
+          button_active_border: "1px solid #43053F",
+          theader_border: "2px solid #43053F",
+          theader_background: "#E7C5E5",
+          entry_border: "2px solid #43053F",
+          entry_background: "#F1DDF0"
+        }
+      }, {
+        template_name: "Red",
+        css_props: {
+          body_color: "#2D0303",
+          body_background_color: "#F1E7E7",
+          hr_color: "1px solid #2D0303",
+          a_color: "#FF0000",
+          a_visited_color: "#FF0000",
+          a_hover_color: "#2D0303",
+          button_background_color: "#FF0000",
+          button_color: "#2D0303",
+          button_border: "1px solid #2D0303",
+          button_hover_background_color: "#FD5959",
+          button_hover_color: "#800000",
+          button_hover_border: "1px solid #800000",
+          button_active_background_color: "#800000",
+          button_active_color: "#2D0303",
+          button_active_border: "1px solid #2D0303",
+          theader_border: "2px solid #2D0303",
+          theader_background: "#E4C8C8",
+          entry_border: "2px solid #2D0303",
+          entry_background: "#E9D7D7"
+        }
+      }, {
+        template_name: "Blue",
+        css_props: {
+          body_color: "#040648",
+          body_background_color: "#EBEBFF",
+          hr_color: "1px solid #040648",
+          a_color: "#01059D",
+          a_visited_color: "#01059D",
+          a_hover_color: "#040648",
+          button_background_color: "#6569FF",
+          button_color: "#040648",
+          button_border: "1px solid #040648",
+          button_hover_background_color: "#999BFF",
+          button_hover_color: "#01059D",
+          button_hover_border: "1px solid #01059D",
+          button_active_background_color: "#01059D",
+          button_active_color: "#040648",
+          button_active_border: "1px solid #040648",
+          theader_border: "2px solid #040648",
+          theader_background: "#C2C3E6",
+          entry_border: "2px solid #040648",
+          entry_background: "#D7D8ED"
+        }
+      }, {
+        template_name: "Tan",
+        css_props: {
+          body_color: "#232303",
+          body_background_color: "#EAEADA",
+          hr_color: "1px solid #232303",
+          a_color: "#676741",
+          a_visited_color: "#676741",
+          a_hover_color: "#232303",
+          button_background_color: "#9E9F65",
+          button_color: "#232303",
+          button_border: "1px solid #232303",
+          button_hover_background_color: "#C0C09A",
+          button_hover_color: "#676741",
+          button_hover_border: "1px solid #676741",
+          button_active_background_color: "#676741",
+          button_active_color: "#232303",
+          button_active_border: "1px solid #232303",
+          theader_border: "2px solid #232303",
+          theader_background: "#D2D2BE",
+          entry_border: "2px solid #232303",
+          entry_background: "#DFDFD3"
+        }
+      }, {
+        template_name: "Teal",
+        css_props: {
+          body_color: "#033736",
+          body_background_color: "#CBEFEF",
+          hr_color: "1px solid #033736",
+          a_color: "#056968",
+          a_visited_color: "#056968",
+          a_hover_color: "#033736",
+          button_background_color: "#01D3D1",
+          button_color: "#033736",
+          button_border: "1px solid #033736",
+          button_hover_background_color: "#05FFFC",
+          button_hover_color: "#019997",
+          button_hover_border: "1px solid #019997",
+          button_active_background_color: "#019997",
+          button_active_color: "#033736",
+          button_active_border: "1px solid #033736",
+          theader_border: "2px solid #033736",
+          theader_background: "#99D9D8",
+          entry_border: "2px solid #033736",
+          entry_background: "#C4E4E4"
+        }
+      }]
     }
   }
 }
--- src/components/js/utilities.js
+++ src/components/js/utilities.js
@@ -1,11 +1,4 @@
-/*
-The Utilities class contains many important utilities methods that are called at various points
-throughout the application. They help manipulate string data that is used in conducting video
-searches, for insert/update queries, and for converting into objects/arrays for further use.
-The methods also assist in counting the number of occurrences of values in arrays, comparing
-two sets of arrays' values, and shuffling values inside arrays. This small utilities library
-helps in cleaning, screening, and analyzing much of the application's data.
-*/
+import XRegExp from 'xregexp';
 
 class Utilities {
 
@@ -29,19 +22,19 @@ class Utilities {
         var searched = search_lyrics.join(" ");
         searched = searched.toLowerCase();
         searched = searched.trim();
-        song_string = song_string.replace(/[^A-Za-z0-9"\s]/g, "");
+        song_string = this.keepAllLettersNumbersSpacesQuotes(song_string);
         song_string = song_string.toLowerCase();
         song_string = song_string.trim();
 
         if (property_name === "video_lyrics" || property_name === "video_tags") {
-          let searchedRegExp = new RegExp(`\\b${searched}\\b`, 'i');
+          let searchedRegExp = XRegExp(`(^|[^\\pL])(${searched})(?![\\pL])`, 'gi');
 
           if (searchedRegExp.test(song_string) === true) {
 
             matches.push(songs_lyrics[j]);
           }
         } else {
-          let searchedRegExp = new RegExp(`^\\b${searched}\\b$`, 'i');
+          let searchedRegExp = XRegExp(`(^|[^\\pL])(${searched})(?![\\pL])`, 'gi');
 
           if (searchedRegExp.test(song_string) === true) {
 
@@ -55,6 +48,7 @@ class Utilities {
       return songs_lyrics;
     }
   }
+
 
   static arrayComparerFindAll(search_lyrics, songs_lyrics, property_name) {
     let search_lyrics_length = search_lyrics.length;
@@ -81,11 +75,11 @@ class Utilities {
             var searched = search_lyrics[i];
             searched = searched.toLowerCase();
             searched = searched.trim();
-            song_string = song_string.replace(/[^A-Za-z0-9"\s]/g, "");
+            song_string = this.keepAllLettersNumbersSpacesQuotes(song_string);
             song_string = song_string.toLowerCase();
             song_string = song_string.trim();
 
-            let searchedRegExp = new RegExp(`\\b${searched}\\b`, 'i');
+            let searchedRegExp = XRegExp(`(^|[^\\pL])(${searched})(?![\\pL])`, 'gi');
 
             if (searchedRegExp.test(song_string) === true) {
 
@@ -106,19 +100,46 @@ class Utilities {
       }
   }
 
+
+  static arrayComparerFindAny(search_lyrics, songs_lyrics, property_name) {
+    let search_lyrics_length = search_lyrics.length;
+    let songs_lyrics_length = songs_lyrics.length;
+
+    if (search_lyrics_length > 0) {
+      let matches = [];
+
+        for (var j = 0; j < songs_lyrics_length; j++) {
+
+          for (var i = 0; i < search_lyrics_length; i++) {
+            if (songs_lyrics[j][property_name] === search_lyrics[i]) {
+              matches.push(songs_lyrics[j]);
+            }
+          }
+        }
+
+        return matches;
+      } else {
+        return songs_lyrics;
+      }
+  }
+
+
   static customSplit(strng) {
-    let test_strng = strng.replace(/[^A-Za-z0-9"]/g, "");
+    let test_strng = this.keepAllLettersNumbersQuotes(strng);
 
     if (test_strng.length > 0) {
-      strng = strng.replace(/[^A-Za-z0-9"\s]/g, "");
+
+      strng = this.keepAllLettersNumbersSpacesQuotes(strng);
+
       strng = strng.toLowerCase();
+
       strng = strng.trim();
 
       let reg_exp = /"(.*?)"/g;
 
       let quotes = strng.match(reg_exp) ? strng.match(reg_exp) : [];
         quotes = quotes.join();
-        quotes = quotes.replace(/[^A-Za-z0-9\s]/g, "");
+        quotes = this.keepAllLettersNumbersSpaces(quotes);
         quotes = quotes.trim();
         quotes = quotes.split(" ");
 
@@ -139,6 +160,7 @@ class Utilities {
       return { quotes: [], notquotes: [] };
     }
   }
+
 
   static yearPruning(songs, min, max) {
     let songs_length = songs.length;
@@ -169,10 +191,10 @@ class Utilities {
           return true;
       });
       return filtering;
-  }
+    }
+
 
     static occurrenceCounter(the_array, test_value, count_property) {
-
       var arguments_length = arguments.length;
 
       if (the_array.length > 0) {
@@ -188,6 +210,7 @@ class Utilities {
         }
 
         var nwarlen = the_array.length;
+
 
         function recursiveCounter() {
 
@@ -233,11 +256,14 @@ class Utilities {
           }
         }
 
+
         recursiveCounter();
+
 
         return occurrences;
       }
     }
+
 
     static arrayShuffler(the_array) {
     	Array.prototype.shuffle = function (old_index, new_index) {
@@ -259,6 +285,7 @@ class Utilities {
 
       let counter = 0;
 
+
       function randomize() {
 
         array_counter = array_counter - 1;
@@ -276,10 +303,12 @@ class Utilities {
         }
       }
 
+
       randomize();
 
       return shuffled_array;
     }
+
 
     static doubleShuffler(first_array, second_array) {
 
@@ -287,6 +316,7 @@ class Utilities {
     		this.splice(new_index, 0, this.splice(old_index, 1)[0]);
     		return this[new_index];
     	};
+
 
       let first_shuffled_array = [];
       let second_shuffled_array = [];
@@ -302,6 +332,7 @@ class Utilities {
       let array_counter = array_length;
 
       let counter = 0;
+
 
       function randomize() {
 
@@ -322,10 +353,40 @@ class Utilities {
         }
       }
 
+
       randomize();
 
       return { first_array: first_shuffled_array, second_array: second_shuffled_array };
     }
+
+  static keepAllLettersNumbers(string) {
+    if (typeof string !== 'string') {
+      string = "";
+    }
+    return XRegExp.replace(string, XRegExp('[^\\s"\\p{N}\\p{L}-]', 'gi'), "");
+  }
+
+  static keepAllLettersNumbersQuotes(string) {
+    if (typeof string !== 'string') {
+      string = "";
+    }
+    return XRegExp.replace(string, XRegExp('[^\\"\\p{N}\\p{L}-]', 'gi'), "");
+  }
+
+  static keepAllLettersNumbersSpaces(string) {
+    if (typeof string !== 'string') {
+      string = "";
+    }
+    return XRegExp.replace(string, XRegExp('[^\\s\\p{N}\\p{L}-]', 'gi'), "");
+  }
+
+  static keepAllLettersNumbersSpacesQuotes(string) {
+    if (typeof string !== 'string') {
+      string = "";
+    }
+    return XRegExp.replace(string, XRegExp('[^\\s"\\p{N}\\p{L}-]', 'gi'), "");
+  }
+
 
   static htmlStringCleanerArrayConverter(string) {
 
@@ -335,7 +396,7 @@ class Utilities {
 
     string= string.replace(/\s+/g, " ");
 
-    string = string.replace(/[^A-Za-z0-9\s]/g,'');
+    string = this.keepAllLettersNumbersSpaces(string);
 
     string = string.toLowerCase();
 
@@ -346,7 +407,9 @@ class Utilities {
     return string;
   }
 
+
   static htmlTagStyleCleaner(tags) {
+
     for (var i = 0; i < tags.length; i++) {
 
       if (tags[i].style.backgroundColor) {
@@ -362,7 +425,7 @@ class Utilities {
       }
 
       if (tags[i].style.fontSize) {
-        tags[i].style.fontSize = '1rem';
+        tags[i].style.fontSize = 'initial';
       }
 
       if (tags[i].style.overflow) {
@@ -385,17 +448,182 @@ class Utilities {
     return tags;
   }
 
-  static removeDangerousTags(innerHTML) {
 
-    innerHTML = innerHTML.replace(/<\/?(?!br)(?!p)(?!pre)(?!div)(?!span)(?!strong)(?!b)(?!u)(?!i)\w*\b[^>]*>/ig, '');
+  static allTags(videos) {
+    let tags = {};
+    let all_tags = [];
+    for (var i = 0, length = videos.length; i < length; i++) {
+      for (var j = 0, jlength = videos[i].video_tags.length; j < jlength; j++) {
+        tags[videos[i].video_tags[j].toLowerCase()] = videos[i].video_tags[j];
+      }
+    }
+    for (var tag in tags) {
+      all_tags.push(tags[tag]);
+    }
+    all_tags.sort(function(a, b) {
+      if (a.toLowerCase() > b.toLowerCase()) {
+        return 1;
+      }
+      if (b.toLowerCase() > a.toLowerCase()) {
+        return -1;
+      }
+    });
+    return all_tags;
+  }
 
-    innerHTML = innerHTML.replace(/\n/ig, '<br>');    
+  static createTagOptions(all_tags) {
+    let tag_options = [];
+    for (var i = 0, length = all_tags.length; i < length; i++) {
+      tag_options.push({ value: all_tags[i], label: all_tags[i] });
+    }
+    return tag_options;
+  }
 
-    innerHTML = innerHTML.replace(/\t/ig, "&nbsp; &nbsp; &nbsp; &nbsp;");
+  static reactSelectStyles(cssTemplate) {
+    return {
+      control: (provided, state) => ({
+        ...provided,
+        width: 300,
+        background: cssTemplate.body_background_color,
+        borderRadius: "3px",
+        borderColor: cssTemplate.body_color,
+        '&:hover': {
+          borderColor: cssTemplate.body_color,
+          background: cssTemplate.entry_background
+        },
+        boxShadow: state.isFocused ? `0 0 0 1px ${cssTemplate.a_color}` : null,
+        cursor: "pointer"
+      }),
+      menu: (provided) => ({
+        ...provided,
+        width: 300,
+        marginTop: 0,
+        background: cssTemplate.body_background_color
+      }),
+      input: (provided) => ({
+        ...provided,
+        color: cssTemplate.body_color
+      }),
+      noOptionsMessage: (provided) => ({
+        ...provided,
+        width: 300,
+        color: cssTemplate.body_color
+      }),
+      placeholder: (provided) => ({
+        ...provided,
+        color: cssTemplate.body_color
+      }),
+      clearIndicator: (provided, state) => ({
+        ...provided,
+        color: state.isFocused ? cssTemplate.body_color : cssTemplate.body_color,
+        '&:hover': {
+          color: state.isFocused ? cssTemplate.a_color : cssTemplate.a_color,
+        }
+      }),
+      dropdownIndicator: (provided, state) => ({
+        ...provided,
+        color: state.isFocused ? cssTemplate.body_color : cssTemplate.body_color,
+        '&:hover': {
+          color: state.isFocused ? cssTemplate.a_color : cssTemplate.a_color,
+        }
+      }),
+      indicatorSeparator: (provided) => ({
+        ...provided,
+        background: cssTemplate.body_color
+      }),
+      loadingIndicator: (provided) => ({
+        ...provided,
+        background: cssTemplate.body_color
+      }),
+      multiValue: (provided) => ({
+        ...provided,
+        background: cssTemplate.body_color
+      }),
+      multiValueLabel: (provided) => ({
+        ...provided,
+        color: cssTemplate.entry_background
+      }),
+      multiValueRemove: (provided) => ({
+        ...provided,
+        color: cssTemplate.entry_background,
+        '&:hover': {
+          color: cssTemplate.body_color,
+          background: cssTemplate.a_color
+        }
+      }),
+      option: (provided, state) => ({
+        ...provided,
+        width: 300,
+        cursor: "pointer",
+        background: state.isFocused ? cssTemplate.entry_background : 'transparent',
+        '&:hover': {
+          background: cssTemplate.entry_background
+        },
+        '&:active': {
+          background: cssTemplate.theader_background
+        }
+      }),
+      valueContainer: (provided) => ({
+        ...provided,
+        width: 300
+      })
+    }
+  }
 
-    innerHTML = innerHTML.replace(/\s+(?![^<>]*>)/ig, "&nbsp;");
+  static generateString() {
+    let characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i",
+    "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C",
+    "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+    "X", "Y", "Z", "_", "-"];
+    let characters_length = characters.length;
+    let string_length = 30;
+    let string_array = [];
+    for (var i = 0; i < string_length; i++) {
+      let rand_el_numb = Math.floor(Math.random() * Math.floor(characters_length));
+      string_array.push(characters[rand_el_numb]);
+    }
+    return string_array.join("");
+  }
 
-    return innerHTML;
+  static fileCorruptionCheck(entries) {
+    let corrupt = false;
+    let counter = [];
+
+    for (var i = 0, length = entries.length; i < length; i++) {
+      if (!entries[i].hasOwnProperty("video_title") === true || !entries[i].hasOwnProperty("video_code") === true || !entries[i].hasOwnProperty("video_band") === true || !entries[i].hasOwnProperty("video_genre") === true || !entries[i].hasOwnProperty("video_lyrics") === true || !entries[i].hasOwnProperty("video_lyrics_html") === true || !entries[i].hasOwnProperty("video_year") === true || !entries[i].hasOwnProperty("video_type") === true || !entries[i].hasOwnProperty("video_tags") === true || !entries[i].hasOwnProperty("video_stars") === true || !entries[i].hasOwnProperty("list_id") === true) {
+        counter.push(1);
+      }
+    }
+
+    if (counter.length > 0) {
+      corrupt = true;
+    }
+
+    return corrupt;
+  }
+
+  static matchDetector(matches) {
+    let new_list_id = this.generateString();
+    function innerRecursiveFunction() {
+      let counter = 0;
+      for (var i = 0, length = matches.length; i < length; i++) {
+        if (new_list_id === matches[i]) {
+          counter = counter + 1;
+        }
+      }
+      if (counter > 0) {
+
+        new_list_id = this.generateString();
+
+        innerRecursiveFunction();
+      } else {
+        matches.push(new_list_id);
+      }
+    }
+
+    innerRecursiveFunction();
+
+    return {matches: matches, new_list_id: new_list_id};
   }
 }
 
