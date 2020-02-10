e28f1a75cf089682177a9fba9e027144af39a2a2
neytjs <35025868+neytjs@users.noreply.github.com>
2020-02-09 21:08:17 -0600

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
-/*
-app.js is the top level of our React interface that calls our central App component and instructs ReactDOM to
-render it in the 'output' div of our index.html.
-https://reactjs.org/docs/react-dom.html
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app-component';
